fdc3408d8264d03922046391fb57153961a5175e
Colin <clnearle@gmail.com>
2023-12-18 07:07:28 -0800

    basemodel udpate

--- models/base_model.py
+++ models/base_model.py
@@ -20,20 +20,21 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        if not kwargs.get('id'):
-            kwargs['id'] = str(uuid.uuid4())
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-        else:
-            for key, value in kwargs.items():
+        if kwargs:
+            for key, val in kwargs.items():
+                 if key == 'updated_at' or key == 'created_at':
+                        val = datetime.strptime(val, '%Y-%m-%dT%H:%M:%S.%f')
                 if key != '__class__':
-                    if key == 'updated_at' or key == 'created_at':
-                        setattr(self, key, datetime.strptime(
-                             value, '%Y-%m-%dT%H:%M:%S.%f'))
-                    else:
-                        setattr(self, key, value)
+                        setattr(self, key, val)
+            if "id" not in kwargs:
+                self.id = str(uuid.uuid4())
+            if "created_at" not in kwargs:
+                self.created_at = datetime.now()
+            if "updated_at" not in kwargs:
+                self.updated_at = datetime.now()
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
 
     def __str__(self):
         """Returns a string representation of the instance"""
