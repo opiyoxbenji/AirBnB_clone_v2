4db31dc2cc4c35bff9f5758b9b07d0761462f3c1
Colin <clnearle@gmail.com>
2023-12-17 22:22:47 -0800

    updated all def in filestorage to take additional arg

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,16 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls is None:
+            return FileStorage.__objects
+        else:
+            filtered_object = {}
+            for key, value in FileStorage.__objects.items():
+                if key.split('.')[0] == cls.__name__:
+                    filtered_objects[key] = value
+            return (filtered_object)
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
