f37ac2ce4d9bfd6dd030f70cab4515607711b639
Jassocc <clnearle@gmail.com>
2024-01-11 05:00:45 -0800

    task 2 and 3

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -57,6 +57,6 @@ def deploy():
     deplouy web_static
     """
     archive_path = do_pack()
-    if not archive_path:
+    if archive_path is None:
         return False
     return do_deploy(archive_path)
