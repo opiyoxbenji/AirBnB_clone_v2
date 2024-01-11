41a31dae4b2c6d7b7d08456603d4585d84a3435d
Jassocc <clnearle@gmail.com>
2024-01-11 01:47:43 -0800

    task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -28,6 +28,7 @@ def do_deploy(archive_path):
         run("rm -rf {}/web_static".format(release_folder))
         run("rm -rf /data/web_static/current")
         run("ln -s {} /data/web_static/current".format(release_folder))
+        print("New version deployed!")
         return True
     except Exception as e:
         return False
