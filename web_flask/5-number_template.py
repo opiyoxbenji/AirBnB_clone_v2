89098935ee739f6dba4c847f1628f36e610ca7ee
opiyoxbenji <opiyosbenji@gmail.com>
2024-01-28 14:50:30 +0000

    an update

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -10,4 +10,38 @@ Routes:
     /number_template/<n> - displays a HTML page only if n is an integer
 """
 from flask import Flask
+app = Flask(__name__)
 
+
+@app.route('/', strict_slashes=False)
+def Hello_HBNB():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_way(text):
+    return 'C ' + text.replace('_', ' ')
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def python(text='is cool'):
+    return 'Python ' + text.replace('_', ' ')
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def int_validator(n):
+    return '{:d} is a number'.format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def html_int_validator(n):
+    return render_template('5-number.html', n=n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
